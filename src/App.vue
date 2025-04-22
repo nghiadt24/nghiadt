f34689e69f751824ad95ebc846fc894190320d4a
Nghĩa Đinh Trọng <nghiadinh24@192.168.1.5>
2025-04-22 20:18:40 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header></header>
+
+  <main></main>
+</template>
+
+<style scoped></style>
