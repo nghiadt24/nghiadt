baa3cb682958dc7c633916a1c68364e639db4f1e
Nghĩa Đinh Trọng <nghiadinh24@192.168.1.7>
2025-04-29 00:52:14 +0700

    update

--- src/components/topNav.vue
+++ src/components/topNav.vue
@@ -14,15 +14,18 @@
           </div>
         </div>
         <div class="col-span-4 contactWrap">
-          <div class="hireStatus">
-            <div class="statusDot"></div>
-            <p class="statusText">Available for hire</p>
-          </div>
+          <button class="hireStatus">
+            <div class="circle">
+              <div class="dot"></div>
+              <div class="outline"></div>
+            </div>
+            Available for works
+          </button>
           <div class="divider"></div>
           <div class="ContactButtonWrap">
-            <button class="contactButton"> <i class="ph-duotone ph-telegram-logo" style="color:#616161"></i> </button>
-            <button class="contactButton"> <i class="ph-duotone ph-linkedin-logo" style="color:#616161"></i> </button>
-            <button class="contactButton"> <i class="ph-duotone ph-behance-logo" style="color:#616161"></i> </button>
+            <button class="contactButton"> <i class="ph-duotone ph-telegram-logo"></i> </button>
+            <button class="contactButton"> <i class="ph-duotone ph-linkedin-logo"></i> </button>
+            <button class="contactButton"> <i class="ph-duotone ph-behance-logo"></i> </button>
           </div>
         </div>
       </div>
@@ -42,10 +45,22 @@
   display: flex;
   padding: 8px;
   align-items: center;
+  color: #616161;
   gap: 10px;
   border-radius: 12px;
   border: 1px solid var(--Button-button-stroke-secondary, #8A8A8A);
   background: var(--Button-button-bg-secondary, #FFF);
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    background: var(--Button-button-bg-primary, #303030);
+    color: white;
+    border-radius: 16px;
+  }
+  &:active {
+    background: var(--Button-button-bg-secondary, #FFF);
+    color: #616161;
+    box-shadow: 0px 0px 0px 2px rgba(124, 124, 124, 0.25);
+  }
 }
 .contactWrap {
   display: flex;
@@ -58,27 +73,123 @@
   height: 40px;
   background: var(--Page-divider-normal, #CCC);
 }
-.statusDot {
-    width: 8px;
-    height: 8px;
-    background: var(--Green---color-green-09, #6AB04C);
-    border-radius: 50%;
+.circle {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  width: 16px;
+  height: 16px;
+  border: solid 2px #178d00;
+  border-radius: 50%;
+  margin: 0 10px;
+  background-color: transparent;
+  animation: circle-keys var(--animation);
+}
+
+.circle .dot {
+  position: absolute;
+  transform: translate(-50%, -50%);
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  background-color: #178d00;
+  animation: dot-keys var(--animation);
+}
+
+.circle .outline {
+  position: absolute;
+  transform: translate(-50%, -50%);
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  animation: outline-keys var(--animation);
+}
+.circle:nth-child(2) {
+  animation-delay: 0.3s;
+}
+.circle:nth-child(2) .dot {
+  animation-delay: 0.3s;
+}
+.circle:nth-child(1) .outline {
+  animation-delay: 0.9s;
+}
+.circle:nth-child(2) .outline {
+  animation-delay: 1.2s;
+}
+@keyframes circle-keys {
+  0% {
+    transform: scale(1);
+    opacity: 1;
   }
-  .statusText {
-    color: var(--Green---color-green-09, #6AB04C);
+
+  50% {
+    transform: scale(1.5);
+    opacity: 0.5;
+  }
+
+  100% {
+    transform: scale(1);
+    opacity: 1;
+  }
+}
+
+@keyframes dot-keys {
+  0% {
+    transform: scale(1);
+  }
+
+  50% {
+    transform: scale(0);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+}
+
+@keyframes outline-keys {
+  0% {
+    transform: scale(0);
+    outline: solid 20px var(--color);
+    outline-offset: 0;
+    opacity: 1;
+  }
+
+  100% {
+    transform: scale(1);
+    outline: solid 0 transparent;
+    outline-offset: 20px;
+    opacity: 0;
+  }
+}
+  .hireStatus {
+    --animation: 2s ease-in-out infinite;
+    display: flex;
+    align-items: center;
+    column-gap: 2px;
+    color: #178d00;
+    background-color: var(--Green---color-green-02, #DBEBDA);
+    border-radius: 100px;
+    padding: 10px 16px;
+    outline: none;
+    border: none;
     font-size: 14px;
     font-style: normal;
     font-weight: 600;
-    line-height: 24px;
-  }
-  .hireStatus {
-    border-radius: 57px;
-    background: var(--Green---color-green-02, #DBEBDA);
-    padding: 8px 16px;
-    display: flex;
-    align-items: center;
-    gap: 6px;
-  }
+    line-height: 22px;
+    position: relative;
+    transition: 0.2s ease-in-out;
+    cursor: pointer;
+    &:hover {
+      background-color: #ffffff;
+    }
+    &:active {
+      background-color: #e1f9dc;
+      border: solid 2px #178d00;
+    }
+    }
+
   .profile {
       display: flex;
       align-items: center;
